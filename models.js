2b17ef3c7791baa85c7a65f5753b1dbfcb551103
walebant <wale.ashiru04@gmail.com>
2023-03-21 23:53:35 +0100

    remove versionKey from schema

--- models.js
+++ models.js
@@ -2,10 +2,13 @@ const mongoose = require('mongoose');
 
 const { Schema } = mongoose;
 
-const BookSchema = new Schema({
-  title: { type: String, required: true },
-  commentcount: { type: Number, default: 0 },
-  comments: { type: Array, default: [String] },
-});
+const BookSchema = new Schema(
+  {
+    title: { type: String, required: true },
+    commentcount: { type: Number, default: 0 },
+    comments: { type: Array, default: [String] },
+  },
+  { versionKey: false }
+);
 
 module.exports = mongoose.model('book', BookSchema);
